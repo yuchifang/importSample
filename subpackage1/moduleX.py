958e9bf5f12b478450db6d3e3f143a16ff8128c7
danfangortery <Dan.Fang@gmail.com>
2022-09-22 11:29:54 +0800

    udpate

--- subpackage1/moduleX.py
+++ subpackage1/moduleX.py
@@ -7,15 +7,19 @@
 
 # 測試 下面的 方法看看
 # https://www.geeksforgeeks.org/python-import-from-parent-directory/
+
+# todo https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
 import sys
 print(sys.version)
 print(sys.version_info)
 print(sys.path)
-# from .moduleY import sample
+# todo 在 webcrawler
 
+# from .moduleY import sample
 # 這種情況 
 # python3 -m subpackage1.moduleX
 # 可以成功
+
 
 from moduleY import sample
 # 這種方法可以 是因為把
